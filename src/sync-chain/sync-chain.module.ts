30bb4810ffc6c67ae4963f438fa234c39b82fe7a
0xRitter <105451784+0xRitter@users.noreply.github.com>
2023-04-30 12:43:42 +0800

    remove GraphQLModule sync-chain module (#181)

--- src/sync-chain/sync-chain.module.ts
+++ src/sync-chain/sync-chain.module.ts
@@ -10,9 +10,6 @@ import { SystemConfigModule } from './system-config/system-config.module';
 import { MintSaleTransactionModule } from './mint-sale-transaction/mint-sale-transaction.module';
 import { MintSaleContractModule } from './mint-sale-contract/mint-sale-contract.module';
 import { Record721Module } from './record721/record721.module';
-import { ApolloDriverConfig, ApolloDriver } from '@nestjs/apollo';
-import { GraphQLModule } from '@nestjs/graphql';
-import { appConfig } from '../lib/configs/app.config';
 
 @Module({
     imports: [
@@ -25,12 +22,6 @@ import { appConfig } from '../lib/configs/app.config';
         SystemConfigModule,
         MintSaleContractModule,
         MintSaleTransactionModule,
-        GraphQLModule.forRoot<ApolloDriverConfig>({
-            driver: ApolloDriver, // GraphQL server adapter
-            debug: appConfig.global.debug ? true : false, // is debug?
-            playground: appConfig.global.debug ? true : false, // is show platground? waiting for fix: throw an error when set it true
-            autoSchemaFile: 'schema.graphql', // schema file(auto generated)
-        }),
         TypeOrmModule.forRoot({
             name: 'sync_chain',
             type: 'postgres',
