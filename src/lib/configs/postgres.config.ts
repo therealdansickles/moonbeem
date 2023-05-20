ba4b2b7ed5f454c0a24e3aebb6a7742a68538613
0xZach <0xzachx@gmail.com>
2023-05-20 09:41:53 +0800

    Zach/db migration (#377)

--- src/lib/configs/postgres.config.ts
+++ src/lib/configs/postgres.config.ts
@@ -3,7 +3,6 @@ import { DataSource } from 'typeorm';
 // we can use this one to replace `lib/configs/db.config.ts`
 // but for now, let's split the config into two files
 // if migrate works, i can do the refactor it soon
-
 export default new DataSource({
     type: 'postgres',
     migrationsTableName: 'migrations',
