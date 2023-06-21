0f6df4cccc4f8c65300e0b5303d3a4fd27a350b3
0xZach <0xzachx@gmail.com>
2023-06-21 17:17:02 +0800

    zach/clain messi shoes (#514)

--- src/redeem/redeem.resolver.ts
+++ src/redeem/redeem.resolver.ts
@@ -1,5 +1,6 @@
 import { UseGuards } from '@nestjs/common';
 import { Resolver, Args, Mutation } from '@nestjs/graphql';
+import { Public } from '../session/session.decorator';
 
 import { Redeem, CreateRedeemInput } from './redeem.dto';
 import { RedeemService } from './redeem.service';
@@ -8,6 +9,10 @@ import { RedeemService } from './redeem.service';
 export class RedeemResolver {
     constructor(private readonly redeemService: RedeemService) {}
 
+    // TODO: temp make it public for frontend guys to test
+    // will add up a auth by address decorator
+    // need to be fix before 2023/06/23
+    @Public()
     @Mutation(() => Redeem, { description: 'Claim a new redeem.' })
     async createRedeem(@Args('input') input: CreateRedeemInput): Promise<Redeem> {
         return await this.redeemService.createRedeem(input);
