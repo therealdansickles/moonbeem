4784b7858ff31910142ca3a4c58787d04383eee4
0xZach <0xzachx@gmail.com>
2023-07-06 14:58:49 +0800

    refactor: use MetadataProperty dto in `NFT` module

--- src/nft/nft.dto.ts
+++ src/nft/nft.dto.ts
@@ -1,28 +1,11 @@
-import { Field, InputType, ObjectType, PickType } from '@nestjs/graphql';
 import { IsInt, IsObject, IsOptional, IsString } from 'class-validator';
+import { GraphQLJSONObject } from 'graphql-type-json';
+import { MetadataProperty } from 'src/metadata/metadata.dto';
+
+import { Field, InputType, ObjectType, PickType } from '@nestjs/graphql';
 
 import { Collection } from '../collection/collection.dto';
-import { GraphQLJSONObject } from 'graphql-type-json';
 import { Tier } from '../tier/tier.dto';
-
-@ObjectType()
-export class MetadataProperty {
-    @IsString()
-    @Field({ description: 'The name of the property.' })
-    readonly name: string;
-
-    @IsString()
-    @Field({ description: 'The type of the property.' })
-    readonly type: string;
-
-    @IsString()
-    @IsOptional()
-    @Field({ nullable: true, description: 'The display value of the property.' })
-    readonly display_type?: string;
-
-    @Field(() => String, { description: 'The value of the property.' })
-    readonly value: string | number;
-}
 
 @ObjectType('Nft')
 export class Nft {
