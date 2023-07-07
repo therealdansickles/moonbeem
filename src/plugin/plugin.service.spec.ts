76f8dfe9620879da13d4cea7972c66faf0362707
0xZach <0xzachx@gmail.com>
2023-07-07 16:54:39 +0800

    feat: add `installOnTier` service

--- src/plugin/plugin.service.spec.ts
+++ src/plugin/plugin.service.spec.ts
@@ -4,7 +4,13 @@ import { faker } from '@faker-js/faker';
 import { Test, TestingModule } from '@nestjs/testing';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { Collection, CollectionKind } from '../collection/collection.entity';
+import { CollectionModule } from '../collection/collection.module';
+import { CollectionService } from '../collection/collection.service';
 import { postgresConfig } from '../lib/configs/db.config';
+import { Tier } from '../tier/tier.entity';
+import { TierModule } from '../tier/tier.module';
+import { TierService } from '../tier/tier.service';
 import { Plugin } from './plugin.entity';
 import { PluginModule } from './plugin.module';
 import { PluginService } from './plugin.service';
@@ -12,6 +18,8 @@ import { PluginService } from './plugin.service';
 describe('PluginService', () => {
     let pluginRepository: Repository<Plugin>;
     let pluginService: PluginService;
+    let collectionService: CollectionService;
+    let tierService: TierService;
 
     beforeAll(async () => {
         const module: TestingModule = await Test.createTestingModule({
@@ -33,13 +41,21 @@ describe('PluginService', () => {
                     logging: false,
                     dropSchema: true,
                 }),
-                PluginModule
+                PluginModule,
+                CollectionModule,
+                TierModule,
             ],
         }).compile();
 
         pluginRepository = module.get('PluginRepository');
         pluginService = module.get<PluginService>(PluginService);
-    })
+        tierService = module.get<TierService>(TierService);
+        collectionService = module.get<CollectionService>(CollectionService);
+    });
+
+    afterAll(async () => {
+        global.gc && global.gc();
+    });
 
     describe('#getPlugins', () => {
         it('should get plugins', async () => {
@@ -97,5 +113,99 @@ describe('PluginService', () => {
         });
     });
 
-    
+    describe('#installOnTier', () => {
+        let collection;
+        let tier;
+
+        beforeEach(async () => {
+            collection = await collectionService.createCollection({
+                name: faker.company.name(),
+                displayName: 'The best collection',
+                about: 'The best collection ever',
+                artists: [],
+                tags: [],
+                kind: CollectionKind.edition,
+                address: faker.finance.ethereumAddress(),
+            });
+
+            tier = await tierService.createTier({
+                name: faker.company.name(),
+                totalMints: 100,
+                collection: { id: collection.id },
+                price: '100',
+                tierId: 0,
+                metadata: {
+                    properties: {
+                        level: {
+                            name: 'level',
+                            type: 'string',
+                            value: 'basic',
+                            display_value: 'Basic',
+                        },
+                    },
+                },
+            });
+        });
+
+        it('should work', async () => {
+            const plugin = await pluginRepository.save({
+                name: faker.commerce.productName(),
+                displayName: faker.commerce.productName(),
+                description: faker.commerce.productDescription(),
+                author: faker.commerce.department(),
+                version: faker.git.commitSha(),
+                metadata: {
+                    properties: {
+                        holding_days: {
+                            name: 'holding_days',
+                            type: 'number',
+                            value: 0,
+                            display_value: '0'
+                        }
+                    },
+                    conditions: {
+                        operator: 'and',
+                        rules: [
+                            {
+                                property: 'holding_days',
+                                rule: 'greater_than',
+                                value: -1,
+                                update: [{
+                                    property: 'holding_days',
+                                    action: 'increase',
+                                    value: 1
+                                }]
+                            },
+                            {
+                                property: 'holding_days',
+                                rule: 'less_than',
+                                value: 999,
+                                update: [{
+                                    property: 'holding_days',
+                                    action: 'increase',
+                                    value: 1
+                                }]
+                            },
+                        ],
+                        trigger: [
+                            {
+                                type: 'schedule',
+                                updatedAt: new Date().toISOString(),
+                                config: {
+                                    startAt: new Date().toISOString(),
+                                    endAt: new Date().toISOString(),
+                                    every: 1,
+                                    unit: 'minutes'
+                                },
+                            },
+                        ],
+                    }
+                }
+            });
+            const result = await pluginService.installOnTier({ tier, plugin });
+            expect(result.metadata.conditions).toBeTruthy();
+            expect(result.metadata.conditions.rules).toEqual(2);
+            expect(result.metadata.conditions.trigger.length).toEqual(1);
+        });
+    });
 })
--- src/plugin/plugin.service.ts
+++ src/plugin/plugin.service.ts
@@ -1,22 +1,13 @@
-import { merge, sort, unique } from 'lodash';
+import { merge, uniq } from 'lodash';
 import { Repository } from 'typeorm';
 
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 
-import { Collection } from '../collection/collection.entity';
 import { CollectionService } from '../collection/collection.service';
 import { MetadataInput } from '../metadata/metadata.dto';
-import { Asset721 } from '../sync-chain/asset721/asset721.entity';
-import { Coin } from '../sync-chain/coin/coin.entity';
-import { MintSaleContract } from '../sync-chain/mint-sale-contract/mint-sale-contract.entity';
-import {
-    MintSaleTransaction
-} from '../sync-chain/mint-sale-transaction/mint-sale-transaction.entity';
 import { Tier as TierDto } from '../tier/tier.dto';
-import { Tier } from '../tier/tier.entity';
 import { TierService } from '../tier/tier.service';
-import { Wallet } from '../wallet/wallet.entity';
 import { Plugin } from './plugin.entity';
 
 @Injectable()
@@ -51,16 +42,16 @@ export class PluginService {
      * @param payload
      * @returns
      */
-    async installOnTier(payload: { tier: TierDto, plugin: Plugin, metadata: MetadataInput}) {
+    async installOnTier(payload: { tier: TierDto, plugin: Plugin, metadata?: MetadataInput}) {
         const { tier, plugin, metadata } = payload;
         const { uses = [], properties = {}, conditions = {} } = tier.metadata;
         const metadataPayload = {
             // add plugin name on uses
-            uses: sort(unique(uses.push(plugin.name))),
+            uses: uniq(uses.push(plugin.name)).sort(),
             // merge properties
-            properties: merge(properties, metadata.properties),
+            properties: merge(properties, plugin.metadata.properties),
             // merge conditions
-            conditions: merge(conditions, metadata.conditions)
+            conditions: merge(conditions, plugin.metadata.conditions, metadata?.conditions)
         }
         await this.tierService.updateTier(tier.id, { metadata: metadataPayload });
         return this.tierService.getTier(tier.id)
