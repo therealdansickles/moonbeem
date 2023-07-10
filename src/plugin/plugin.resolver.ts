9203cdc6cb45eb7f42bdd7be6656f00b5dc34206
0xZach <0xzachx@gmail.com>
2023-07-10 11:07:30 +0800

    feat: add `installOnCollection` service

--- src/plugin/plugin.resolver.ts
+++ src/plugin/plugin.resolver.ts
@@ -2,6 +2,7 @@ import { GraphQLError } from 'graphql';
 
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 
+import { CollectionService } from '../collection/collection.service';
 import { Public } from '../session/session.decorator';
 import { TierService } from '../tier/tier.service';
 import { InstallOnCollectionInput, InstallOnTierInput, Plugin } from './plugin.dto';
@@ -11,6 +12,7 @@ import { PluginService } from './plugin.service';
 export class PluginResolver {
     constructor(
         private readonly pluginService: PluginService,
+        private readonly collectionService: CollectionService,
         private readonly tierService: TierService,
     ) {}
 
@@ -27,7 +29,19 @@ export class PluginResolver {
     }
 
     @Mutation()
-    async installOnCollection(@Args('input') input: InstallOnCollectionInput) {}
+    async installOnCollection(@Args('input') input: InstallOnCollectionInput) {
+        const collection = await this.collectionService.getCollection(input.collectionId);
+        if (!collection) throw new GraphQLError(`Collection ${input.collectionId} doesn't exsit.`);
+
+        const tiers = await this.tierService.getTiersByCollection(input.collectionId);
+        if (!tiers || tiers.length === 0)  throw new GraphQLError(`Collection ${input.collectionId} doesn't have tiers.`);
+        
+        const plugin = await this.pluginService.getPlugin(input.pluginId);
+        if (!plugin) throw new GraphQLError(`Plugin ${input.pluginId} doesn't exist.`);
+
+        const promises = tiers.map(tier => this.pluginService.installOnTier({ tier, plugin, metadata: input.metadata }));
+        return Promise.all(promises);
+    }
 
     @Mutation()
     async installOnTier(@Args('input') input: InstallOnTierInput) {
