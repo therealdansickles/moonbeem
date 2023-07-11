6c3eee66f37d3a3d6aa03d635b15cf5b23e09da6
0xZach <0xzachx@gmail.com>
2023-07-11 08:14:54 +0800

    fix: add missing `plugin.module` in app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,37 +1,38 @@
 import * as dotenv from 'dotenv'; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
-
-import { APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
-import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { RavenInterceptor, RavenModule } from 'nest-raven';
+
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
+import { GraphQLModule } from '@nestjs/graphql';
+import { JwtModule } from '@nestjs/jwt';
+import { ScheduleModule } from '@nestjs/schedule';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { CollaborationModule } from './collaboration/collaboration.module';
 import { CollectionModule } from './collection/collection.module';
-import { GraphQLModule } from '@nestjs/graphql';
-import { JwtModule } from '@nestjs/jwt';
-import { MembershipModule } from './membership/membership.module';
-import { Module } from '@nestjs/common';
 import { MongoAdapter } from './lib/adapters/mongo.adapter';
+import { RedisAdapter } from './lib/adapters/redis.adapter';
+import { appConfig } from './lib/configs/app.config';
+import { postgresConfig } from './lib/configs/db.config';
+import { MembershipModule } from './membership/membership.module';
+import { UploadModule } from './modules/upload.module';
 import { MoonpayModule } from './moonpay/moonpay.module';
 import { NftModule } from './nft/nft.module';
 import { OpenseaModule } from './opensea/opensea.module';
 import { OrganizationModule } from './organization/organization.module';
+import { PluginModule } from './plugin/plugin.module';
 import { PollerModule } from './poller/poller.module';
 import { RedeemModule } from './redeem/redeem.module';
-import { RedisAdapter } from './lib/adapters/redis.adapter';
 import { RelationshipModule } from './relationship/relationship.module';
-import { ScheduleModule } from '@nestjs/schedule';
+import { SaleHistoryModule } from './saleHistory/saleHistory.module';
 import { SearchModule } from './search/search.module';
 import { SessionGuard } from './session/session.guard';
 import { SessionModule } from './session/session.module';
 import { SyncChainModule } from './sync-chain/sync-chain.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { UploadModule } from './modules/upload.module';
 import { UserModule } from './user/user.module';
 import { WaitlistModule } from './waitlist/waitlist.module';
 import { WalletModule } from './wallet/wallet.module';
-import { appConfig } from './lib/configs/app.config';
-import { postgresConfig } from './lib/configs/db.config';
-import { SaleHistoryModule } from './saleHistory/saleHistory.module';
 
 dotenv.config();
 
@@ -55,6 +56,10 @@ dotenv.config();
         MoonpayModule,
         RedeemModule,
         NftModule,
+        OpenseaModule,
+        MoonpayModule,
+        SaleHistoryModule,
+        PluginModule,
         // integration graphql
         GraphQLModule.forRoot<ApolloDriverConfig>({
             driver: ApolloDriver, // GraphQL server adapter
@@ -75,9 +80,6 @@ dotenv.config();
             secret: process.env.SESSION_SECRET,
             signOptions: { expiresIn: '1d' },
         }),
-        OpenseaModule,
-        MoonpayModule,
-        SaleHistoryModule,
     ],
     providers: [
         {
