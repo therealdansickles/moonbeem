5a462ec67235a2c416ab152025bd0ea8b12d6735
Arthur Chiu <24772+achiurizo@users.noreply.github.com>
2023-09-13 15:18:15 -0700

    Simplify DB Config (#867)
    Co-authored-by: Arthur Chiu <achiurizo@users.noreply.github.com>

--- src/lib/configs/db.config.ts
+++ src/lib/configs/db.config.ts
@@ -2,18 +2,8 @@ import * as dotenv from 'dotenv'; // see https://github.com/motdotla/dotenv#how-
 dotenv.config();
 
 export const postgresConfig = {
-    url: process.env.V1_DATABASE_URL,
-    host: process.env.V1_DATABASE_HOST,
-    port: process.env.V1_DATABASE_PORT ? parseInt(process.env.V1_DATABASE_PORT) : 5432,
-    username: process.env.V1_DATABASE_USERNAME,
-    password: process.env.V1_DATABASE_PASSWORD,
-    database: process.env.V1_DATABASE_NAME,
+    url: process.env.DATABASE_URL,
     syncChain: {
         url: process.env.SYNC_CHAIN_DATABASE_URL,
-        host: process.env.SYNC_CHAIN_DATABASE_HOST,
-        port: process.env.SYNC_CHAIN_DATABASE_PORT ? parseInt(process.env.SYNC_CHAIN_DATABASE_PORT) : 5432,
-        username: process.env.SYNC_CHAIN_DATABASE_USERNAME,
-        password: process.env.SYNC_CHAIN_DATABASE_PASSWORD,
-        database: process.env.SYNC_CHAIN_DATABASE_NAME,
     },
 };
