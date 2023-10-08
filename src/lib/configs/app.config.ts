cf1a41f35aeefe26ca19a4bc5a7ca145d1a8311f
Hong Liang <lianghongbuaa@gmail.com>
2023-10-08 16:50:45 +0800

    fix deployment (#936)

--- src/lib/configs/app.config.ts
+++ src/lib/configs/app.config.ts
@@ -4,7 +4,7 @@ export const appConfig = {
         host: process.env.HOST || 'localhost',
         port: process.env.PORT || 3000,
         debug: process.env.NODE_ENV === 'dev' ?? false,
-        sql_logging: process.env.SQL_LOGGING_ENABLED ?? false,
+        sql_logging: !!process.env.SQL_LOGGING_ENABLED,
     },
     cron: {
         disabled: Boolean(process.env.DISABLE_CRON),
