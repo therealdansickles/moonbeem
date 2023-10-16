ad26a371385ed708c179c181dd6ab196848804e7
alfred <lianghongbuaa@gmail.com>
2023-10-16 16:00:16 +0800

    add analytics api

---
+++ src/analytics/analytics.module.ts
@@ -0,0 +1,121 @@
+import { HttpModule } from '@nestjs/axios';
+import { forwardRef, Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule, JwtService } from '@nestjs/jwt';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { AlchemyWebhook } from '../alchemy/alchemy-webhook.entity';
+import { AlchemyModule } from '../alchemy/alchemy.module';
+import { AlchemyService } from '../alchemy/alchemy.service';
+import { CoinMarketCapModule } from '../coinmarketcap/coinmarketcap.module';
+import { CoinMarketCapService } from '../coinmarketcap/coinmarketcap.service';
+import { Collaboration } from '../collaboration/collaboration.entity';
+import { CollaborationModule } from '../collaboration/collaboration.module';
+import { CollectionPlugin } from '../collectionPlugin/collectionPlugin.entity';
+import { CollectionPluginModule } from '../collectionPlugin/collectionPlugin.module';
+import { CollectionPluginService } from '../collectionPlugin/collectionPlugin.service';
+import { MaasModule } from '../maas/maas.module';
+import { MaasService } from '../maas/maas.service';
+import { MailModule } from '../mail/mail.module';
+import { Membership } from '../membership/membership.entity';
+import { MembershipModule } from '../membership/membership.module';
+import { MembershipService } from '../membership/membership.service';
+import { MerkleTree } from '../merkleTree/merkleTree.entity';
+import { Nft } from '../nft/nft.entity';
+import { NftModule } from '../nft/nft.module';
+import { NftService } from '../nft/nft.service';
+import { OpenseaModule } from '../opensea/opensea.module';
+import { OpenseaService } from '../opensea/opensea.service';
+import { Organization } from '../organization/organization.entity';
+import { OrganizationModule } from '../organization/organization.module';
+import { Plugin } from '../plugin/plugin.entity';
+import { Redeem } from '../redeem/redeem.entity';
+import { Asset721 } from '../sync-chain/asset721/asset721.entity';
+import { Asset721Module } from '../sync-chain/asset721/asset721.module';
+import { Asset721Service } from '../sync-chain/asset721/asset721.service';
+import { Coin } from '../sync-chain/coin/coin.entity';
+import { CoinModule } from '../sync-chain/coin/coin.module';
+import { CoinService } from '../sync-chain/coin/coin.service';
+import { History721 } from '../sync-chain/history721/history721.entity';
+import { History721Module } from '../sync-chain/history721/history721.module';
+import { MintSaleContract } from '../sync-chain/mint-sale-contract/mint-sale-contract.entity';
+import { MintSaleContractModule } from '../sync-chain/mint-sale-contract/mint-sale-contract.module';
+import { MintSaleTransaction } from '../sync-chain/mint-sale-transaction/mint-sale-transaction.entity';
+import { MintSaleTransactionModule } from '../sync-chain/mint-sale-transaction/mint-sale-transaction.module';
+import { MintSaleTransactionService } from '../sync-chain/mint-sale-transaction/mint-sale-transaction.service';
+import { Tier } from '../tier/tier.entity';
+import { TierModule } from '../tier/tier.module';
+import { TierService } from '../tier/tier.service';
+import { User } from '../user/user.entity';
+import { UserModule } from '../user/user.module';
+import { UserService } from '../user/user.service';
+import { Wallet } from '../wallet/wallet.entity';
+import { WalletModule } from '../wallet/wallet.module';
+import { Collection } from '../collection/collection.entity';
+import { CollectionService } from '../collection/collection.service';
+import { AnalyticsService } from './analytics.service';
+import { AnalyticsResolver } from './analytics.resolver';
+
+@Module({
+    imports: [
+        HttpModule,
+        TypeOrmModule.forFeature([
+            Collaboration,
+            Collection,
+            Organization,
+            Membership,
+            Tier,
+            Nft,
+            Wallet,
+            User,
+            Redeem,
+            Plugin,
+            MerkleTree,
+            CollectionPlugin,
+            AlchemyWebhook,
+        ]),
+        TypeOrmModule.forFeature([Coin, MintSaleContract, MintSaleTransaction, Asset721, History721], 'sync_chain'),
+        forwardRef(() => Asset721Module),
+        forwardRef(() => CoinMarketCapModule),
+        forwardRef(() => CoinModule),
+        forwardRef(() => CollaborationModule),
+        forwardRef(() => MailModule),
+        forwardRef(() => MembershipModule),
+        forwardRef(() => MintSaleContractModule),
+        forwardRef(() => MintSaleTransactionModule),
+        forwardRef(() => OpenseaModule),
+        forwardRef(() => OrganizationModule),
+        forwardRef(() => TierModule),
+        forwardRef(() => UserModule),
+        forwardRef(() => WalletModule),
+        forwardRef(() => History721Module),
+        forwardRef(() => NftModule),
+        forwardRef(() => AlchemyModule),
+        forwardRef(() => CollectionPluginModule),
+        forwardRef(() => MaasModule),
+        JwtModule,
+        ConfigModule,
+    ],
+    exports: [AnalyticsModule, AnalyticsService],
+    providers: [
+        JwtService,
+        CoinService,
+        MembershipService,
+        OpenseaService,
+        TierService,
+        UserService,
+        CollectionService,
+        CoinMarketCapService,
+        MintSaleTransactionService,
+        NftService,
+        Asset721Service,
+        ConfigService,
+        AlchemyService,
+        CollectionPluginService,
+        MaasService,
+        AnalyticsService,
+        AnalyticsResolver,
+    ],
+    controllers: [],
+})
+export class AnalyticsModule {}
--- src/test-utils.ts
+++ src/test-utils.ts
@@ -12,7 +12,6 @@ import { RoyaltyService } from './sync-chain/royalty/royalty.service';
 import { TierService } from './tier/tier.service';
 import { MembershipService } from './membership/membership.service';
 import { CreateMembershipInput } from './membership/membership.dto';
-import { gql } from './user/user.resolver.spec';
 import * as request from 'supertest';
 import { INestApplication } from '@nestjs/common';
 import { Repository } from 'typeorm';
@@ -21,9 +20,19 @@ import { MerkleTree } from './merkleTree/merkleTree.entity';
 import { MerkleTreeType } from './merkleTree/merkleTree.dto';
 import { MerkleTreeService } from './merkleTree/merkleTree.service';
 import { CollectionPluginService } from './collectionPlugin/collectionPlugin.service';
+import { Nft } from './nft/nft.entity';
+import { User } from './user/user.entity';
+import { UserService } from './user/user.service';
 
 const collectionPluginService: CollectionPluginService = global.collectionPluginService;
 const pluginRepository: Repository<Plugin> = global.pluginRepository;
+const collectionService: CollectionService = global.collectionService;
+const tierService: TierService = global.tierService;
+const userService: UserService = global.userService;
+const nftRepository: Repository<Nft> = global.nftRepository;
+const walletRepository: Repository<User> = global.walletRepository;
+
+export const gql = String.raw;
 
 export const createCoin = async (coinService: CoinService, coin?: any) =>
     coinService.createCoin({
@@ -49,6 +58,17 @@ export const createCollection = async (collectionService: CollectionService, col
         ...collection,
     });
 
+export const createCollection2 = async (collection?: any) =>
+    collectionService.createCollection({
+        name: faker.string.sample(10),
+        displayName: faker.company.name(),
+        about: faker.company.name(),
+        tags: [],
+        kind: CollectionKind.edition,
+        address: faker.finance.ethereumAddress(),
+        ...collection,
+    });
+
 export const createTier = async (tierService: TierService, tier?: any) =>
     tierService.createTier({
         name: faker.company.name(),
@@ -59,6 +79,42 @@ export const createTier = async (tierService: TierService, tier?: any) =>
         tierId: 0,
         metadata: {},
         ...tier,
+    });
+
+export const createTier2 = async (tier?: any) =>
+    tierService.createTier({
+        name: faker.company.name(),
+        totalMints: 100,
+        collection: { id: faker.string.uuid() },
+        price: '100',
+        paymentTokenAddress: faker.finance.ethereumAddress(),
+        tierId: 0,
+        metadata: {},
+        ...tier,
+    });
+
+export const createNft = async (collectionId, tierId, nft?: any) =>
+    nftRepository.save({
+        collectionId,
+        tierId,
+        tokenId: '9',
+        properties: {},
+        ...nft,
+    });
+
+export const createUser = async (user?: any) =>
+    userService.createUser({
+        username: faker.internet.userName(),
+        email: faker.internet.email(),
+        password: 'password',
+        ...user,
+    });
+
+export const createWallet = async (wallet?: any) =>
+    walletRepository.save({
+        address: faker.finance.ethereumAddress(),
+        name: faker.internet.userName(),
+        ...wallet,
     });
 
 export const createMintSaleContract = async (contractService: MintSaleContractService, contract?: any) =>
@@ -173,7 +229,6 @@ export const createPlugin2 = async (plugin?: any) =>
         ...plugin,
     });
 
-
 export const createPlugin = async (repo: Repository<Plugin>, plugin?: any) =>
     repo.save({
         name: faker.commerce.productName(),
@@ -185,7 +240,11 @@ export const createPlugin = async (repo: Repository<Plugin>, plugin?: any) =>
         ...plugin,
     });
 
-export const createRecipientsMerkleTree = async (merkleTreeService: MerkleTreeService, collectionAddress: string, tokenIds: number[]): Promise<MerkleTree> => {
+export const createRecipientsMerkleTree = async (
+    merkleTreeService: MerkleTreeService,
+    collectionAddress: string,
+    tokenIds: number[],
+): Promise<MerkleTree> => {
     const data = tokenIds.map((tokenId) => {
         return { collection: collectionAddress, tokenId, quantity: '1' };
     });
@@ -204,7 +263,7 @@ export const createCollectionPlugin = async (collectionId: string, pluginId: str
             tokenAddress: faker.finance.ethereumAddress(),
         },
         merkleRoot: faker.string.hexadecimal({ length: 66, casing: 'lower' }),
-        ...collectionPlugin
+        ...collectionPlugin,
     });
 };
 
@@ -233,8 +292,7 @@ export const getToken = async (app: INestApplication, email: string) => {
         },
     };
 
-    const tokenRs = await request(app.getHttpServer()).post('/graphql').send(
-        { query: tokenQuery, variables: tokenVariables });
+    const tokenRs = await request(app.getHttpServer()).post('/graphql').send({ query: tokenQuery, variables: tokenVariables });
 
     const { token } = tokenRs.body.data.createSessionFromEmail;
     return token;
